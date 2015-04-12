30c7ff2f9ac6ad513b28f746696607a662b59dcf
Nitzan <nitzan.h.sherman@gmail.com>
2015-04-12 08:01:05 -0400

    fourth plot

---
+++ plot4.R
@@ -0,0 +1,22 @@
+full_file <- read.csv("household_power_consumption.txt",sep = ";",
+                      colClasses = c("character","character","numeric","numeric","numeric","numeric",
+                                     "numeric","numeric","numeric"),na.strings = "?")
+
+#Subset Data to only what I want
+clean_data <- subset(full_file,Date == "1/2/2007" | Date == "2/2/2007")
+#Create DateTime column to be used for plot
+clean_data$DateTime = paste(clean_data$Date,clean_data$Time)
+clean_data$DateTime = strptime(clean_data$DateTime, "%d/%m/%Y %H:%M:%S")
+#launch file device and plot
+png(filename = "plot4.png",height = 480, width = 480)
+par(mfrow = c(2,2))
+plot(x = clean_data$DateTime, y = clean_data$Global_active_power,type = "l",xlab = "",ylab = "Global Active Power")
+plot(x = clean_data$DateTime, y = clean_data$Voltage,type = "l",xlab = "datetime",ylab = "Voltage")
+plot(x= clean_data$DateTime,y = clean_data$Sub_metering_1,col = "black",type = "l",xlab = "",ylab = "Energy Sub metering")
+lines(x = clean_data$DateTime,y = clean_data$Sub_metering_2, col = "red")
+lines(x = clean_data$DateTime,y = clean_data$Sub_metering_3, col = "blue")
+legend("topright",border = "white",c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty = 1, 
+       col = c("black", "red","blue"),cex = .7)
+plot(x = clean_data$DateTime, y = clean_data$Global_reactive_power,type = "l",xlab = "datetime",ylab = "Global_reactive_power")
+#turn off png device
+dev.off()
